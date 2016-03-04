798c5e3a7c37d9357731d55c2bd07bf4231cd08f
wesslnelson <wesley.nelson@gordon.edu>
2016-03-04 12:11:08 -0500

    Added learnable moves to playa detail

--- www/js/controller.js
+++ www/js/controller.js
@@ -4,14 +4,11 @@ theDataBank.controller("MatchupCtrl", function($scope, PlayersV1, DamRecCalculat
   // all types are created as objects within an object and contain a
   // name, weaknesses, resistances, and immunities
   $scope.Players = PlayersV1.getPlayers();
-
   //initialized as empty because default type is not assigned
   var type1= "";
   var currentDRC=DamRecCalculator.emptyDRC();
-
   //create an object to lookup types by string name
   var lookup = PlayersV1.getLookUp();
-
   //function called when CALC button is pressed
   $scope.calculateAll = function(tName) {
     setType1(tName);
@@ -23,7 +20,6 @@ theDataBank.controller("MatchupCtrl", function($scope, PlayersV1, DamRecCalculat
   calculateWRI = function() {
     //initializes varies categories types could fall into
     var weaknesses  = []; var resistances = []; var neutralities  = [];
-
     for (var prop in currentDRC) {
       if (currentDRC[prop] == 2) {weaknesses.push(prop);}
       else if (currentDRC[prop] == 0.5) {resistances.push(prop);}
@@ -33,23 +29,21 @@ theDataBank.controller("MatchupCtrl", function($scope, PlayersV1, DamRecCalculat
     $scope.weaknesses  = weaknesses;
     $scope.resistances = resistances;
     $scope.neutralities  = neutralities;
-
   };
-
   setType1 = function(tName) {type1 = $scope.Players[lookup[tName]]};
   $scope.setPlayer = function(t1) {
     setType1(t1);
   };
 })
 
-
 theDataBank.controller("PlayersCtrl", function($scope, PlayersV1) {
   $scope.Players = PlayersV1.getPlayers();
-
 })
 
-theDataBank.controller("PlayerDetailCtrl", function($scope, $stateParams, PlayersV1) {
+theDataBank.controller("PlayerDetailCtrl", function($scope, $stateParams, PlayersV1, MovesV1) {
   $scope.playa = PlayersV1.getPlayer($stateParams.playa);
+  $scope.Moves = MovesV1;
+
 })
 
 
